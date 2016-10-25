8b951e86490035eaf01b121d53745ccacf3823ba
Luka Harabajsa <luka.harii@gmail.com>
2016-10-25 18:42:58 +0200

    Lesson 2 i 3

---
+++ lesson-02/01.php
@@ -0,0 +1,11 @@
+<?php
+
+//header('Content-Type: text/plain');
+
+$o = new StdClass();
+$o->x = 'Just a property';
+
+var_dump($o);
+
+
+
---
+++ lesson-02/02-operators.php
@@ -0,0 +1,39 @@
+<?php
+
+
+//Zbrajanje
+/*
+$n = 1;
+$f = 2.55;
+var_dump($n+$f);
+*/
+
+/*
+$s = 'PHP akademija';
+$s = $s .' u Osijeku';
+var_dump($s);
+*/
+
+/*
+$a = [
+    1,
+    2,
+    'one' => 'a1',
+    'two' =>'a2'
+    
+ ];
+
+$b = [
+    2,
+    3,
+    'two' => 'b2',
+    'three' => 'b3'
+    
+    
+];
+
+var_dump($a+$b);
+var_dump(array_merge($a, $b));
+var_dump(array_replace($a,$b));
+ * 
+ */
